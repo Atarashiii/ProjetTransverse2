e25181392200727a7c137fbbdbabfad996325c09
Atarashiii <newtheo67@gmail.com>
2024-06-28 10:23:14 +0200

    update

--- config/config.php
+++ config/config.php
@@ -15,7 +15,7 @@ $database = new Database($servername, $username, $password, $dbname);
 $conn = $database->getConnection();
 
 if (!defined('APP_VERSION')) {
-    define('APP_VERSION', '1.5.0');
+    define('APP_VERSION', '1.5.2');
 }
 
 ?>
